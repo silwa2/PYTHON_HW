ed4d2ebeef14990616f1026462ef2ddac683a603
Aleksander Sinkov <silwa2@yandex.ru>
2023-06-29 13:10:20 +0300

    Modul_Task_sem_5

---
+++ Seminar_6/Task_Seminar/Module/Module_Task_sem_5.py
@@ -0,0 +1,31 @@
+import random
+
+def puzzle(puzle_text: str, solutions: list[str], tries: int) -> int:
+    print(puzle_text)
+    solutions =list(map(lambda x: x.lower(), solutions))
+    num = 0
+    while num < tries:
+        user_input = input("введит вариант ответа: ").lower()
+        if user_input in solutions:
+            num = num + 1
+            return num
+        else:
+            print('не угадал!')
+        num += 1
+    return 0
+
+
+'''
+Задача 5
+Добавьте в модуль с загадками функцию, которая хранит словарь списков.
+Ключ словаря - загадка, значение - список с отгадками.
+Функция в цикле вызывает загадывающую функцию, чтобы передать ей все свои загадки.
+
+'''
+
+
+def puzzle_solut():
+    dict_puzzle = {'Зимой и летом одним цветом': ['ель', 'елка', 'доллар'], 'Висит груша - нельзя скушать': ['груша', 'игрушка', 'лампочка']}
+
+    for key, values in dict_puzzle.items():
+        puzzle(key, values, random.randint(1, 5))
