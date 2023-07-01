5b5b4422f64a7f60dd58931f8f46b65f30343de0
silwa2 <silwa2@yandex.ru>
2023-07-01 14:57:48 +0300

    Seminar_6_Task_3

---
+++ Seminar_6/Task_HW/Task_3.py
@@ -0,0 +1,9 @@
+# Напишите функцию в шахматный модуль.
+# Используйте генератор случайных чисел для случайной расстановки ферзей в задаче выше.
+# Проверяйте различный случайные  варианты и выведите 4 успешных расстановки.
+import random
+from Seminar_6.Task_HW.Module_HW.Module_Task_HW_3 import conflict,queens,queenprint
+print('Всего успешных расстоновок: ' + str(len(list(queens()))))
+print()
+print('Один вариант успешной расстановки:')
+queenprint(random.choice(list(queens())))
