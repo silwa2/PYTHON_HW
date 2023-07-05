04c0482eb2f9ad619a346e430c3acab9d6eea334
Aleksander Sinkov <silwa2@yandex.ru>
2023-07-05 15:35:40 +0300

    Task_7

---
+++ Seminar_7/Task_HW/Homework/Task/Task_7.py
@@ -0,0 +1,4 @@
+
+from Seminar_7.Task_HW.Homework.Module.Module_Task_7 import group_file_in_dir
+group_file_in_dir('C:\\Users\\79282\\PycharmProjects\\PYTHON_HW\\Seminar_7\\Task_Seminar\\Zorro', \
+                      'C:\\Users\\79282\PycharmProjects\\PYTHON_HW\\Seminar_7\\Task_Seminar\\Rezult')
