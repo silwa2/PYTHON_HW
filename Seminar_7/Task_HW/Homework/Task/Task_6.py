cfca6fc4c6aa27b1fd1b1f5be5c0ecaf42c06760
Aleksander Sinkov <silwa2@yandex.ru>
2023-07-05 15:35:26 +0300

    Task_6

---
+++ Seminar_7/Task_HW/Homework/Task/Task_6.py
@@ -0,0 +1,5 @@
+
+from Seminar_7.Task_HW.Homework.Module.Module_Task_6 import expansion_files, create_dir
+create_dir('Zorro')
+my_dict = {'.rar': 1, '.torrent': 1, '.avi': 1, '.pdf': 1}
+expansion_files(my_dict)
