a9bad5882480b43293b3551bf5f1095d55a57577
Aleksander Sinkov <silwa2@yandex.ru>
2023-07-10 11:26:17 +0300

    Seminar_8_Task_7

---
+++ Seminar_8/HW_Task/Task_7.py
@@ -0,0 +1,28 @@
+'''
+Задание №7
+Прочитайте созданный в прошлом задании csv файл без использования csv.DictReader.
+Распечатайте его как pickle строку.
+'''
+import csv
+
+import csv
+import pickle
+
+def csv_reader(file):
+    with open(file, encoding='utf-8') as r_file:
+        # Создаем объект reader, указываем символ-разделитель ","
+        file_reader = csv.reader(r_file, delimiter=",")
+        count = 0
+        # Считывание данных из CSV файла
+        for row in file_reader:
+            if count == 0:
+                print(f'Файл содержит столбцы: {", ".join(row)}')
+            else:
+                print(f'{row[0]} {row[1]} {row[2]}')
+            count += 1
+        print(f'Всего в файле {count} строк.')
+        str_pickle = pickle.dumps(file)
+        print(str_pickle)
+
+
+csv_reader('file.csv')
